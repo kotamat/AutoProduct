777bf3ede828749711c0a4711dcaebe2ce2a5189
Kota Matsumoto <kota1861@gmail.com>
2023-05-16 18:39:54 +0900

    上書きされてしまったので修正

--- chatgpt.ts
+++ chatgpt.ts
@@ -1,19 +1,35 @@
-import { Application } from './application.ts';
+const OPENAI_API_KEY = Deno.env.get("OPENAI_API_KEY");
+async function chatGPT(prompt: string): Promise<string> {
+  const response = await fetch(
+    "https://api.openai.com/v1/chat/completions",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "gpt-4",
+        // model: "gpt-3.5-turbo",
+        messages: [{ "role": "user", "content": prompt }],
+        max_tokens: 2000,
+        temperature: 0.5,
+      }),
+    },
+  );
 
-export class ChatGPT {
-  constructor() {}
+  if (response.ok) {
+    const json = await response.json();
 
-  async summarizeAndSendSlack(application: Application) {
-    const summary = await this.summarize(application);
-    await this.sendSlack(summary);
-  }
+    return json.choices[0].message.content.trim();
+  } else {
+    console.log("prompt: ", prompt);
 
-  private async summarize(application: Application): Promise<string> {
-    // Perform summarization using ChatGPT API
-    return 'Summary of the application';
-  }
-
-  private async sendSlack(summary: string): Promise<void> {
-    // Send summary to Slack using Slack API
+    throw new Error(
+      `ChatGPT API request failed: ${response.statusText}: ${await response
+        .text()}}`,
+    );
   }
 }
+
+export { chatGPT };
